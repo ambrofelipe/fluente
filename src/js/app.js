94b1e8831751a64278dd6ec4e5d43940f522e794
Felipe Ambrosio <felipe@fluente.me>
2021-06-11 12:54:42 +0100

    Removed jQuery. Cut JS size to a third. Compressed video a bit more - halved its size.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,93 @@
 import "../sass/main.scss";
 
-import "jquery";
-
 // Plugins
+
+import { CountUp } from "countup.js";
+
+// Executa somente na página index - to-do: code splitting
+let indexPage = document.querySelector(".teams");
+if (indexPage) {
+	let target;
+
+	// Aguarda a página carregar para definir o alvo e iniciar o observador
+	window.addEventListener(
+		"load",
+		(event) => {
+			target = document.querySelector(".contador");
+
+			createObserver();
+		},
+		false
+	);
+
+	// Define o observador
+	function createObserver() {
+		let observer;
+
+		let options = {
+			root: null, // null observa com relação ao viewport
+			rootMargin: "-100px 0", // espera o contador passar 100px da margem inferior
+			threshold: 1.0, // inicia quando estiver 100% visível
+		};
+
+		observer = new IntersectionObserver(iniciaContador, options);
+		observer.observe(target);
+	}
+
+	// Define o callback chamado pelo observador
+	function iniciaContador(entries, observer) {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				let options = {
+					duration: 4,
+				};
+				let experiencia = new CountUp("anosExperiencia", 6, options);
+				let alunos = new CountUp("numAlunos", 150, options);
+
+				if (!experiencia.error && !alunos.error) {
+					experiencia.start();
+					alunos.start();
+				} else {
+					console.error(experiencia.error);
+					console.error(alunos.error);
+				}
+			}
+		});
+	}
+}
+
 import Data from "./particles.json";
 import "particles.js";
 
-import "./custom";
+import Typewriter from "typewriter-effect/dist/core";
+
+// Executa somente na página portfolio - to-do: code splitting
+let portfolioPage = document.getElementById("main-portfolio");
+if (portfolioPage) {
+	// PARTICLES EFFECT IN PORTFOLIO
+	// Inicia particles.js
+	particlesJS.load("main-portfolio", "js/particles.json");
+
+	// TYPEWRITER EFFECT IN PORTFOLIO
+	let typePortfolio = document.getElementById("typeHeader");
+
+	var typewriter = new Typewriter(typePortfolio, {
+		loop: false,
+		delay: 75,
+	});
+	typewriter
+		.pauseFor(2500)
+		.typeString("I'm a web deveolper.")
+		.pauseFor(900)
+		.deleteChars(6)
+		.pauseFor(400)
+		.typeString("loper.")
+		.pauseFor(1500)
+		.deleteChars(19)
+		.pauseFor(900)
+		.typeString(" make the internet more fun")
+		.pauseFor(400)
+		.typeString(" and accessible.")
+		.pauseFor(1000)
+		.start();
+}
--- src/js/custom.js
+++ src/js/custom.js
@@ -5,49 +5,41 @@
 // var carousel = new bootstrap.Carousel(myCarousel);
 
 // COUNT-UP IN INDEX
-let contadorClass = document.getElementsByClassName("conta").length;
-if (contadorClass > 0) {
-	var a = 0;
+// let contadorClass = document.getElementsByClassName("conta").length;
+// if (contadorClass > 0) {
+// 	var a = 0;
 
-	$(function () {
-		$(window).on("scroll", function () {
-			var doTopo = $(".info").offset().top - window.innerHeight;
-			if (a == 0 && $(window).scrollTop() > doTopo) {
-				$(".conta").each(function () {
-					var $this = $(this),
-						contaAte = $this.attr("data-count");
-					$({
-						contaDe: $this.text(),
-					}).animate(
-						{
-							contaDe: contaAte,
-						},
+// 	$(function () {
+// 		$(window).on("scroll", function () {
+// 			var doTopo = $(".info").offset().top - window.innerHeight;
+// 			if (a == 0 && $(window).scrollTop() > doTopo) {
+// 				$(".conta").each(function () {
+// 					var $this = $(this),
+// 						contaAte = $this.attr("data-count");
+// 					$({
+// 						contaDe: $this.text(),
+// 					}).animate(
+// 						{
+// 							contaDe: contaAte,
+// 						},
 
-						{
-							duration: 3000,
-							easing: "swing",
-							step: function () {
-								$this.text(Math.floor(this.contaDe));
-							},
-							complete: function () {
-								$this.text(this.contaDe);
-							},
-						}
-					);
-				});
-				a = 1;
-			}
-		});
-	});
-}
-
-// PARTICLES EFFECT IN PORTFOLIO
-let particlesCanvas = document.getElementsByClassName("main-portfolio").length;
-if (particlesCanvas > 0) {
-	particlesJS.load("particles-js", "js/particles.json", function () {
-		console.log("callback - particles.js config loaded");
-	});
-}
+// 						{
+// 							duration: 3000,
+// 							easing: "swing",
+// 							step: function () {
+// 								$this.text(Math.floor(this.contaDe));
+// 							},
+// 							complete: function () {
+// 								$this.text(this.contaDe);
+// 							},
+// 						}
+// 					);
+// 				});
+// 				a = 1;
+// 			}
+// 		});
+// 	});
+// }
 
 // TEMA
 
